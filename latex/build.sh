9341c2aa9f7d17b32cffb4188848f29d0d75f273
Jorge Castro Gallardo <jcastro@ujaen.es>
2018-06-21 17:41:34 +0200

    Avoid carriage return

--- latex/build.sh
+++ latex/build.sh
@@ -1,18 +1,1 @@
-textfile="qa-context.tex"
-auxfile="qa-context.aux"
-
-rm *.aux
-
-pdflatex $textfile
-bibtex    $auxfile
-pdflatex $textfile
-pdflatex $textfile
-
-rm diff*
-echo "Creating diff file..."
-~/latexdiff/latexdiff old.tex $textfile > diff.tex
-
-pdflatex diff.tex
-bibtex    diff.aux
-pdflatex diff.tex
-pdflatex diff.tex
+textfile="qa-context.tex";auxfile="qa-context.aux";rm *.aux;pdflatex $textfile;bibtex    $auxfile;pdflatex $textfile;pdflatex $textfile;rm diff*;echo "Creating diff file...";~/latexdiff/latexdiff old.tex $textfile > diff.tex;pdflatex diff.tex;bibtex   diff.aux;pdflatex diff.tex;pdflatex diff.tex;
