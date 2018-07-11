fcf2090278245a4695aefff129e227ade7ebadae
Jorge Castro Gallardo <jcastro@ujaen.es>
2018-07-11 15:43:43 +0200

    Update makediff.sh to include diff

--- latex/makediff.sh
+++ latex/makediff.sh
@@ -1,1 +1,2 @@
-textfile="qa-context.tex";auxfile="qa-context.aux";rm *.aux;pdflatex $textfile;bibtex    $auxfile;pdflatex $textfile;pdflatex $textfile;
+textfile="qa-context.tex";auxfile="qa-context.aux";rm *.aux;pdflatex $textfile;bibtex    $auxfile;pdflatex $textfile;pdflatex $textfile;rm diff.*;~/latexdiff/latexdiff old.tex $textfile > diff.tex; pdflatex diff.tex;bibtex diff.aux;pdflatex diff.tex; pdflatex diff.tex;
+
